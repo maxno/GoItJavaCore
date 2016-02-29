8e141622dc3434e82b6a68878ee4ee5bbac51d30
Dmytro Olshanskyi <olshanskyi@gmail.com>
2016-02-29 23:42:48 +0200

    First home task.

---
+++ src/ua/com/goit/dolshanskyi/module1/Dz1.java
@@ -0,0 +1,8 @@
+package ua.com.goit.dolshanskyi.module1;
+
+public class Dz1 {
+    public static void main(String[] args){
+
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
