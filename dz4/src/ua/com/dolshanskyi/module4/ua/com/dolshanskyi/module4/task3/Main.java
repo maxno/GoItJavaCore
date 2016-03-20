03cc505aa0708511bceef51a7c54d399f089847e
Dmytro Olshanskyi <olshanskyi@gmail.com>
2016-03-20 23:08:40 +0200

    Исправил замечания.

--- dz4/src/ua/com/dolshanskyi/module4/ua/com/dolshanskyi/module4/task3/Main.java
+++ dz4/src/ua/com/dolshanskyi/module4/ua/com/dolshanskyi/module4/task3/Main.java
@@ -1,9 +1,15 @@
 package ua.com.dolshanskyi.module4.ua.com.dolshanskyi.module4.task3;
+
 public class Main {
+
     public static void main(String[] args){
+
         Point firstPoint = new Point(3, 6);
         Point secondPoint = new Point(5, 9);
+
         double distsnce = Math.sqrt((Math.pow(secondPoint.getX() - firstPoint.getX(), 2)) + (Math.pow(secondPoint.getY() - firstPoint.getY(), 2)));
+
         System.out.println("Distance between two points : " + distsnce);
+
     }
 }
